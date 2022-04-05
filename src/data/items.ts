e0a22b5d51e02e9c9275ed5e195703e80714e3c3
Cyberdrick <rodrigoschort01@gmail.com>
2022-04-05 19:24:41 -0300

    expense-tracker

--- src/data/items.ts
+++ src/data/items.ts
@@ -4,5 +4,7 @@ export const items: Item[] = [
     { date: new Date(2022, 3, 6), category: 'food', title: 'McDonalds', value: 32.12 },
     { date: new Date(2022, 3, 15), category: 'food', title: 'Burger King', value: 28 },
     { date: new Date(2022, 3, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
-    { date: new Date(2021, 10, 20), category: 'salary', title: 'Salário ACME', value: 4500 },
+    { date: new Date(2022, 4, 5), category: 'salary', title: 'Salário ACME', value: 4500 },
+    { date: new Date(2022, 4, 20), category: 'salary', title: 'Freelance', value: 2000 },
+    { date: new Date(2022, 5, 13), category: 'spent', title: 'Revisão HD 883R', value: 500 },
 ];
